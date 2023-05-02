472f3e5d663b523ee58af8c55ace5a3ed92ab877
nsw2022 <tmddn3410@naver.com>
2023-05-02 18:23:45 +0900

    멤버테이블_0502

---
+++ pydb/member.sql
@@ -0,0 +1,17 @@
+--member 테이블
+CREATE TABLE member(
+	memberid	TEXT PRIMARY KEY,
+	password	TEXT NOT NULL,
+	name		TEXT NOT NULL,
+	gender		TEXT,
+	regdate		datetime DEFAULT(datetime('now','localtime'))
+);
+SELECT * from member;
+-- 회원 추가
+INSERT INTO member(memberid, password, name, gender) 
+VALUES('cloul123','m123456#','장그레','남');
+
+INSERT INTO member(memberid, password, name, gender) 
+VALUES('cloul345','m123456#','오대리','여');
+
+DELETE FROM member WHERE gender='남';
